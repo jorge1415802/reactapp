79c148db7c53a9df34511418f94ba21c5cfa936a
Jorge <rjogeluis64@gmail.com>
2020-08-08 11:35:53 -0300

    react

---
+++ precache-manifest.96ec6900b6c7eb3ca6a05a279e04d027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c3c691da5e3b93737e5ef2550f4078",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "8ad95203df82ce87d419",
+    "url": "/reactapp/static/css/main.5b6d918e.chunk.css"
+  },
+  {
+    "revision": "987ca9838ecf7e249e45",
+    "url": "/reactapp/static/js/2.1b64b5a2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactapp/static/js/2.1b64b5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad95203df82ce87d419",
+    "url": "/reactapp/static/js/main.379489dd.chunk.js"
+  },
+  {
+    "revision": "e37986fbab4425b7214d",
+    "url": "/reactapp/static/js/runtime-main.c3f32358.js"
+  }
+]);
